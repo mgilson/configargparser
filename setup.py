88ebc66bed1eaf5ed7f977752c692a78aaac8b3a
Matthew Lee Gilson <matt@getpattern.com>
2017-05-06 17:20:25 -0700

    Update release.
    Pypi will not accept the change otherwise...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='configargparser',
-    version='0.0.1',
+    version='0.0.2',
     author='Matthew Gilson',
     author_email='m.gilson1@gmail.com',
     description='Parse arguments from anywhere',
